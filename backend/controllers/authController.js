cb9708dd8a3d0fb9021f12040c1804c473971146
KunAttila69 <klun.attila0508@gmail.com>
2025-01-29 20:59:21 +0100

    added login function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,8 +13,26 @@ const registerUser = async (req,res) => {
         res.status(201).json({token})
     }
     catch (error) {
-        res.status(400).json({ message: 'User registration failed', error });
+        res.status(400).json({ message: "User registration failed", error });
     }
 }
 
-module.exports = {registerUser}
+const loginUser = async (req,res) => {
+    const {email,password} = req.body
+    try{
+        const user = User.findOne({email})
+
+        if(!user) return res.status(404).json({message: "User not found"})
+
+        const isMatch = bycrypt.compare(password, user.password)
+        if(!isMatch) return res.status(401).json({message: "Invalid credentials"})
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1d"})
+        res.status(200).json({ token });
+    }
+    catch (error){
+        res.status(400).json({ message: "Login failed"}, error)
+    }
+}
+
+module.exports = {registerUser, loginUser}
