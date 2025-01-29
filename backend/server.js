da9afa6c261fa25807df2e61dbd565e0caad2a65
KunAttila69 <klun.attila0508@gmail.com>
2025-01-29 10:33:03 +0100

    added connection to db

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const connectDB = require("./config/db")
 
 const app = express()
-connectDB
+connectDB()
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> {
