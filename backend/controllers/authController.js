68c0b7131dbf2d20eae387af791449e4aba0e0ec
KunAttila69 <klun.attila0508@gmail.com>
2025-01-29 14:27:35 +0100

    fixed registration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,32 +1,20 @@
 const User = require("../models/User")
 const bycrypt = require("bcryptjs")
-const jwt = requier("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 
-exports.registerUser = async (req,res) => {
-    const {name, email, password} = res.body
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({ name, email, password: hashedPassword });
-    
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-        res.status(201).json({ token });
-      } catch (error) {
+const registerUser = async (req,res) => {
+    const {name, email, password} = req.body
+    try{
+        const hashedPassword = await bycrypt.hash(password, 10)
+        const user = await User.create({name,email,password: hashedPassword})
+        
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1d"})
+
+        res.status(201).json({token})
+    }
+    catch (error) {
         res.status(400).json({ message: 'User registration failed', error });
-      }
+    }
 }
 
-exports.loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-      const user = await User.findOne({ email });
-      if (!user) return res.status(404).json({ message: 'User not found' });
-  
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
-  
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-      res.status(200).json({ token });
-    } catch (error) {
-      res.status(400).json({ message: 'Login failed', error });
-    }
-  };
+module.exports = {registerUser}
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-require("dotenv").config()
-const express = require("express")
-const connectDB = require("./config/db")
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db'); 
+const authRouter = require("./routes/authRoutes")
 
-const app = express()
-connectDB()
+const app = express();
+connectDB();
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=> {
-    console.log("App listening on port: "+PORT)
-})
+app.use(express.json());
+app.use('/api/auth', authRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
